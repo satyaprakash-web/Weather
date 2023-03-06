721512218b28ac7cec4b4b8f058662ec2236eb24
Satya Prakash Tiwari <satyaprakash1684@gmail.com>
2023-03-06 20:03:13 +0530

    Weatherapp

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./component/TopButtons";
+import Inputs from "./component/Inputs";
+import TimeandLocation from "./component/TimeandLocation";
+import TemperatureAndDetails from "./component/TemperatureAndDetails";
+import Forecast from "./component/Forecast";
+// import getWeatherData from "./component/services/weatherService";
+import getFormattedWeatherData from "./component/services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [query, setQuery] = useState({ q: "Ranchi" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
+  return (
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 
+    bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-lg shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+      {weather && (
+        <div>
+          <TimeandLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+
+          {/* <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="Daily forecast" items={weather.daily} /> */}
+        </div>
+      )}
+      <ToastContainer
+        autoclose={5000}
+        theme="coloured"
+        newestOnTop={true}
+        style={{ color: "#000" }}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/services/weatherService.js
@@ -0,0 +1,99 @@
+import { DateTime } from "luxon";
+const API_KEY = "d0162c175744e79a2f6b372738b29d54";
+// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
+// https://api.openweathermap.org/data/2.5/weather?q=Garhwa,IN&units=metric&appid=d0162c175744e79a2f6b372738b29d54
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  // url= `${BASE_URL}/weather?q=${}`
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  console.log(url);
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "ccc"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
+
+  hourly = hourly.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+      temp: d.temp,
+      icon: d.weather[0].icon,
+    };
+  });
+
+  return { timezone, daily, hourly };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  // const formattedForecastWeather = await getWeatherData("onecall", {
+  //   lat,
+  //   lon,
+  //   exclude: "current,minutely,alerts",
+  //   units: searchParams.units,
+  // }).then(formatForecastWeather);
+  console.log(formattedCurrentWeather);
+  return {
+    ...formattedCurrentWeather,
+    // ...formattedForecastWeather
+  };
+};
+// npm i luxon
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time:'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconURLFromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconURLFromCode };
